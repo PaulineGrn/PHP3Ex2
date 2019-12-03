b0aa0a5f68bd031f50b09f6ad04e505793237ea8
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-03 15:12:07 +0100

    PHP3Ex2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Déclaration d'une variable month. Array est pour déclarer un tableau -->
+<?php
+$month=array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP3Ex1</title>
+  </head>
+  <body>
+    <?php
+    // Afficher la variable month avec une valeur [5]
+echo$month[5];
+     ?>
+  </body>
+</html>
